4d4cdb3455f623d2e6375d2dd8c1defc82ee217d
Brett Zamir <brettz9@yahoo.com>
2015-08-07 13:48:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ocr-tool
+
+For performing OCR recognition on images.
+
+**This repository is currently just a hard-coded example usage of node-tesseract**
+
+# Installation
+
+1. Install [node-tesseract](https://github.com/tesseract-ocr/tesseract) (e.g., the [Windows installer](https://code.google.com/p/tesseract-ocr/downloads/detail?name=tesseract-ocr-setup-3.02.02.exe&can=2&q=)).
+1. ```npm install .```
+
+# Usage
+
+`node index`
+
+# Todos
+
+1. Stop hard-coding and make an interface, allowing for dynamic control of directory, file count, text processing, etc.
+
---
+++ index.js
@@ -0,0 +1,46 @@
+/*jslint node:true*/
+
+(function () {'use strict';
+
+var fs = require('fs');
+var t = require('node-tesseract');
+
+function pad (val, len) {
+    var arrLen = (len + 1 - val.toString().length);
+    if (arrLen <= 1) {
+        arrLen = 1;
+    }
+    return new Array(arrLen).join('0') + val; // Todo: refactor with String.prototype.repeat
+}
+
+function readOCR (i, max, processCb, doneCb) {
+    t.process('C:\\Users\\Brett\\Documents\\images\\image-' + pad(i, 4) + '.jpg', function (err, text) { // to 0376.jpg
+        if (err) {
+            console.error(err);
+        }
+        else {
+            processCb(text, i);
+            if (i < max) {                
+                readOCR(++i, max, processCb, doneCb);
+                return;
+            }
+            doneCb();
+        }
+    });
+}
+
+var str = '';
+readOCR(1, 376, function (text, i) {
+        console.log('Finishing ' + i);
+        str += '\n\n{{page|' + i + '}}\n\n' + text;
+    }, function () {
+    fs.writeFile(__dirname + 'ocr.txt', str, function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('Saved!');
+    });
+});
+
+
+}());
