055e332eb9c65ba5a2027ce3d144214bf0b536cb
Brett Zamir <brettz9@yahoo.com>
2015-08-12 14:27:14 -0700

    put skip boolean  though set to false

--- convert.js
+++ convert.js
@@ -2,18 +2,21 @@
 
 var volume = process.argv[2];
 var offset = process.argv[3];
+var skip = false;
 
 var ot = require('./index'); // require('ocr-bulk');
 ot.writeFile({
     start: 1,
-    end: 316,
+    end: 256,
     outputPath: __dirname + '/ocr-' + volume + '.txt',
     getImagePath: function (i) {
         return 'C:\\Users\\Brett\\Documents\\images-' + volume + '\\image-' + ot.pad(i, 4) + '.jpg';
     },
     concatenater: function (text, i) {
         console.log('Finishing ' + i);
-        return '\n\n{{page|' + /*(i + (offset ? parseInt(offset, 10) : 0)) +*/ '}}\n\n' + text;
+        return '\n\n{{page|' +
+            (skip ? '' : (i + (offset ? parseInt(offset, 10) : 0))) +
+            '}}\n\n' + text;
     },
     done: function () {
         console.log('Saved!');
