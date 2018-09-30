97f07f1c79ceb86c9842285416eed89839849abe
Brett Zamir <brettz9@yahoo.com>
2018-09-30 22:40:53 +0800

    - Linting (LGTM): Avoid unused var. - Refactoring (Use object shorthand)

--- convert-roman.js
+++ convert-roman.js
@@ -1,25 +1,25 @@
 (function () {'use strict';
 
 const volume = process.argv[2];
-const offset = process.argv[3];
+// const offset = process.argv[3];
 const skip = false;
 
-const toRoman = require('roman-numerals').toRoman;
+const {toRoman} = require('roman-numerals');
 const ot = require('./index'); // require('ocr-bulk');
 ot.writeFile({
     start: 1,
     end: 15,
     outputPath: __dirname + '/ocr-' + volume + '.txt',
-    getImagePath: function (i) {
+    getImagePath (i) {
         return 'C:\\Users\\Brett\\Documents\\images-' + volume + '\\BW_vol3pg' + toRoman(i).toLowerCase() + '.jpg';
     },
-    concatenater: function (text, i) {
+    concatenater (text, i) {
         console.log('Finishing ' + i);
         return '\n\n{{page|' +
             (skip ? '' : (toRoman(i).toLowerCase())) +
             '}}\n\n' + text;
     },
-    done: function () {
+    done () {
         console.log('Saved!');
     }
 });
